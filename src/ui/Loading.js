c7ae5781adc923bc415db36d15b5f04aea99b89a
Taeho Kim <dittos@gmail.com>
2017-05-23 00:18:51 +0900

    Use glamorous for overriding spinner style

--- src/ui/Loading.js
+++ src/ui/Loading.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import { Spinner } from '@blueprintjs/core';
+import g from 'glamorous';
 
-export default Spinner;
+const Wrapper = g.div({
+  '& .pt-spinner': {
+    position: 'absolute', // Fix for WebKit (don't know why)
+    top: '50%',
+    left: '50%',
+    marginLeft: '-25px',
+    marginTop: '-25px',
+  }
+});
+
+export default function Loading() {
+  return <Wrapper><Spinner /></Wrapper>;
+}
