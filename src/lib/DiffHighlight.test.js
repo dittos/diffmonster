75a3f45174ab6d62386666b5b4d0ba7d4d1a58ae
Taeho Kim <dittos@gmail.com>
2017-07-08 18:53:24 +0900

    Fix/remove tests

--- src/lib/DiffHighlight.test.js
+++ src/lib/DiffHighlight.test.js
@@ -1,59 +1,66 @@
-import { parsePatch } from './PatchParser';
+import { parseDiff } from './DiffParser';
 import { highlightDiff } from './DiffHighlight';
+
+function parse(block) {
+  return parseDiff(`diff --git a b
+--- a
++++ b
+${block}`)[0].blocks[0];
+}
 
 describe('DiffHighlight', () => {
   it('highlights the beginning of a line', () => {
-    const patch = parsePatch(`@@ -1,3 +1,3 @@
+    const patch = parse(`@@ -1,3 +1,3 @@
  aaa
 -bbb
 +0bb
- ccc`)[0];
+ ccc`);
     expect(highlightDiff(patch)).toMatchSnapshot();
   });
 
   it('highlights the end of a line', () => {
-    const patch = parsePatch(`@@ -1,3 +1,3 @@
+    const patch = parse(`@@ -1,3 +1,3 @@
  aaa
 -bbb
 +bb0
- ccc`)[0];
+ ccc`);
     expect(highlightDiff(patch)).toMatchSnapshot();
   });
 
   it('highlights the middle of a line', () => {
-    const patch = parsePatch(`@@ -1,3 +1,3 @@
+    const patch = parse(`@@ -1,3 +1,3 @@
  aaa
 -bbb
 +b0b
- ccc`)[0];
+ ccc`);
     expect(highlightDiff(patch)).toMatchSnapshot();
   });
 
   it('does not highlight whole line', () => {
-    const patch = parsePatch(`@@ -1,3 +1,3 @@
+    const patch = parse(`@@ -1,3 +1,3 @@
  aaa
 -bbb
 +000
- ccc`)[0];
+ ccc`);
     expect(highlightDiff(patch)).toMatchSnapshot();
   });
 
   it('does not highlight boring pairs', () => {
-    const patch = parsePatch(`@@ -1,3 +1,3 @@
+    const patch = parse(`@@ -1,3 +1,3 @@
  aaa
 -  
 +     
- ccc`)[0];
+ ccc`);
     const result = highlightDiff(patch);
     expect(result.some(({ content }) => content.some(part => part.highlight))).toBe(false);
   });
 
   it('does not highlight mismatched hunk size', () => {
-    const patch = parsePatch(`@@ -1,3 +1,3 @@
+    const patch = parse(`@@ -1,3 +1,3 @@
  aaa
 -bbb
 +b0b
-+ccc`)[0];
++ccc`);
     const result = highlightDiff(patch);
     expect(result.some(({ content }) => content.some(part => part.highlight))).toBe(false);
   });
