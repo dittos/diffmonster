8be60bcb632bedab58a1811d47ec3c212cd0d514
Taeho Kim <dittos@gmail.com>
2017-07-09 21:52:20 +0900

    redux-devtools

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import getInitialState from './getInitialState';
 import pullRequestReducer, { pullRequestEpic } from './PullRequestStore';
@@ -20,6 +20,9 @@ function reducer(state = getInitialState(), action) {
 
 export function configureStore() {
   const epicMiddleware = createEpicMiddleware(rootEpic);
-  const store = createStore(reducer, applyMiddleware(epicMiddleware));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(epicMiddleware)
+  ));
   return store;
 }
