a8a3ca2e9a78f31e24957205fc6b479b870d07dd
Taeho Kim <dittos@gmail.com>
2020-06-13 22:52:36 +0900

    Update Blueprint

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,11 @@ import { HashRouter as Router } from 'react-router-dom';
 import App from './routes/App';
 import './index.css';
 import config from './config';
+import { FocusStyleManager } from '@blueprintjs/core';
 
 firebase.initializeApp(config.firebase);
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
   <Router>
